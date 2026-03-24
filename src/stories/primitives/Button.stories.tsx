978465a6fe1655c49256cd008a67564b658839ae
Kewal Shah <kewal.shah@acuitybrands.com>
2026-03-24 16:31:59 -0400

    feat: add storybook mapping

--- src/stories/primitives/Button.stories.tsx
+++ src/stories/primitives/Button.stories.tsx
@@ -1,3 +1,4 @@
+import { figma } from "@figma/code-connect";
 import type { Meta, StoryObj } from "@storybook/react";
 import { IconActivity, IconArrowLeft } from "icons";
 import { Button, ButtonDanger, ButtonGroup } from "primitives";
@@ -8,33 +9,6 @@ const meta: Meta<typeof Button> = {
   parameters: { layout: "centered" },
 };
 export default meta;
-
-export const StoryButton: StoryObj<typeof Button> = {
-  name: "Button",
-  args: {
-    children: "Hello world",
-    variant: "primary",
-  },
-  argTypes: {
-    children: {
-      control: { type: "text" },
-    },
-    size: {
-      control: { type: "select" },
-    },
-    variant: {
-      control: { type: "select" },
-      options: ["primary", "neutral", "subtle"],
-    },
-  },
-  render: ({ children, ...props }) => (
-    <Button {...props}>
-      <IconArrowLeft />
-      {children}
-      <IconActivity />
-    </Button>
-  ),
-};
 
 export const StoryButtonDanger: StoryObj<typeof ButtonDanger> = {
   name: "Button Danger",
@@ -81,3 +55,43 @@ export const StoryButtonGroup: StoryObj<typeof ButtonGroup> = {
     </ButtonGroup>
   ),
 };
+
+
+export const StoryButton: StoryObj<typeof Button> = {
+  name: "Button",
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/design/e6Sjmed60NI77axindCYY3?node-id=4185-3778",
+      examples: [StoryButtonDanger],
+      props: {
+        label: figma.string("Text Content"),
+        disabled: figma.boolean("Disabled"),
+      },
+    },
+  },
+  args: {
+    children: "Hello world",
+    variant: "primary",
+  },
+  argTypes: {
+    children: {
+      control: { type: "text" },
+    },
+    size: {
+      control: { type: "select" },
+    },
+    variant: {
+      control: { type: "select" },
+      options: ["primary", "neutral", "subtle"],
+    },
+  },
+  render: ({ children, ...props }) => (
+    <Button {...props}>
+      <IconArrowLeft />
+      {children}
+      <IconActivity />
+    </Button>
+  ),
+};
+
